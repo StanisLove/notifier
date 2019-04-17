b132d39399f7e6734a91028c18b6b35972ae1b87
Artem Pyankov <artpyankov@gmail.com>
2019-04-17 17:37:55 +0300

    Add pushwoosh

--- lib/notifier/notifier_router.ex
+++ lib/notifier/notifier_router.ex
@@ -3,9 +3,10 @@ defmodule Notifier.Router do
   use Plug.Debugger
   require Logger
 
-  plug(Plug.Logger, log: :debug)
-  plug(:match)
-  plug(:dispatch)
+  plug Plug.Static, at: "/", from: "front/public"
+  plug Plug.Logger, log: :debug
+  plug :match
+  plug :dispatch
 
   get "/hello" do
     send_resp(conn, 200, "world")
