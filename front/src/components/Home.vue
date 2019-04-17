a76ec96d20dae3bec52b5559b01eca7df705a5a0
Stanislav Kravchenko <stankra@yandex.ru>
2019-04-17 14:36:15 +0300

    Add slack form

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -2,29 +2,28 @@
 <div class="hero is-fullheight is-info is-bold">
 <div class="hero-body">
 <div class="container">
-
-  <h1 class="title has-text-centered">{{ message }}</h1>
-  <div class="box">
+  <h1 class="title has-text-center">{{ message }}</h1>
+  <div class="box slack-box">
     <!-- slack form -->
-    <form id="signup-form">
-      <!-- name -->
+    <h2 class="title has-text-center">Slack</h2>
+    <form id="slack-form" @submit.prevent="processForm">
+      <!-- webhook -->
       <div class="field">
-        <label class="label">Name</label>
-        <input type="text" class="input" name="name">
+        <label class="label has-text-left">Webhook</label>
+        <input type="text" class="input" name="webhook" v-model="webhook">
       </div>
 
-      <!-- email -->
+      <!-- text -->
       <div class="field">
-        <label class="label">Email</label>
-        <input type="email" class="input" name="email">
+        <label class="label has-text-left">Message</label>
+        <textarea type="text" class="textarea" rows="10" name="text" v-model="text" />
       </div>
 
       <!-- submit button -->
-      <div class="field has-text-right">
+      <div class="field has-text-left">
         <button type="submit" class="button is-danger">Submit</button>
       </div>
     </form>
-
   </div>
 
 </div>
@@ -38,9 +37,11 @@ import {HTTP} from '../http-common';
 
 export default {
   name: 'Home',
+  el: '#slack-form',
   data: () => ({
-    postBody: 'empty',
-    errors: [],
+    webhook: '',
+    text: '',
+    errors: []
   }),
   props: {
     message: String
@@ -52,17 +53,19 @@ export default {
         alert(event.target.tagName)
       }
     },
-    postPost() {
-      axios.post(`http://127.0.0.1:8085`, { body: this.postBody })
-        .then(response => {})
-        .catch(e => { this.errors.push(e) })
+    processForm() {
+      axios.post(
+        'http://127.0.0.1:8085/post',
+        { type: 'slack', text: this.text, options: { webhook: this.webhook } },
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+      ).then(response => {}).catch(e => { this.errors.push(e) })
     }
   }
 }
 </script>
 
 <style scoped>
-.slack {
+.slack-box {
   background-color: green;
 }
 </style>
